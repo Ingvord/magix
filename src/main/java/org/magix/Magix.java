92adfd4e07f0e38d33c2faa3195c99c2b9d859c3
ingvord <igor.khokhriakov@rcsb.org>
2022-11-07 16:45:11 -0800

    Progress

--- src/main/java/org/magix/Magix.java
+++ src/main/java/org/magix/Magix.java
@@ -7,10 +7,12 @@ import io.micronaut.http.sse.Event;
 import io.reactivex.rxjava3.core.Flowable;
 import io.reactivex.rxjava3.processors.MulticastProcessor;
 
+import java.util.Map;
+
 @Controller("/magix/api")
 public class Magix {
 
-    private final MulticastProcessor<Event<String>> eventFlowableProcessor = MulticastProcessor.create();
+    private final MulticastProcessor<Event<Object>> eventFlowableProcessor = MulticastProcessor.create();
 
     {
         eventFlowableProcessor.start();
@@ -18,16 +20,16 @@ public class Magix {
 
     @Get("/subscribe")
     @Produces(MediaType.TEXT_EVENT_STREAM)
-    public Flowable<Event<String>> subscribe(){
+    public Flowable<Event<Object>> subscribe(){
         return eventFlowableProcessor;
     }
 
 
     @Post("/broadcast")
-    @Consumes(MediaType.TEXT_PLAIN)
-    public void broadcast(String message){
+    public void broadcast(Object message, @QueryValue(defaultValue = "message") String channel){
         eventFlowableProcessor.onNext(
                 Event.of(message)
+                        .name(channel)
         );
     }
 }
